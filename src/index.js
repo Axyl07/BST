ed66574730b079b0be545c09c7ebef133335e619
Axyl07 <aryanchauhan000777@gmail.com>
2025-02-06 22:10:53 +0530

    Driver Script implemented and verified

--- src/index.js
+++ src/index.js
@@ -9,54 +9,47 @@ function generateRandomArray(max,min=0) {
     return array;
 }
 
-console.log(generateRandomArray(10));
-
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-// const tree1 = new Tree([1,2,3,4,5]);
-console.log(tree);
-// console.log(tree1.root);
-
-// tree.deleteItem(8)
-tree.insert(45645)
-tree.insert(45646)
-tree.insert(45647)
-tree.insert(45648)
-tree.insert(45649)
-tree.insert(6)
-tree.insert(2)
-tree.insert(0)
-// tree.deleteItem(15)
-// tree.deleteItem(3) 
-let f67 = tree.find(67);
-let f6345 = tree.find(6345);
-console.log(f67);
-console.log(tree.find(324, tree.root));
-
-// tree.levelOrder((node) => {
-//     console.log(node.data);
-// });
-
-tree.preOrder((node) => {
-    console.log(node.data);
-})
-
-tree.inOrder((node) => {
-    console.log(node.data);
-})
-
-tree.postOrder((node) => {
-    console.log(node.data);
-})
-// console.log(tree.height(tree.root));
-// console.log(tree.height(f67));
-// console.log(tree.height(tree.find(4)));
-console.log(tree.isBalanced());
-tree.prettyPrint(tree.root);
-tree.rebalance();
-console.log(tree.isBalanced());
-tree.prettyPrint(tree.root);
-console.log(tree.depth(f6345));
-
-console.log(tree);
-
-
+let testArray1 = generateRandomArray(10);
+let testTree = new Tree(testArray1);;
+// testTree.levelOrder((node) => {
+    //     console.log(node);
+    // })
+    // testTree.preOrder((node) => {
+        //     console.log(node);
+        // })
+        // testTree.inOrder((node) => {
+            //     console.log(node);
+// })
+// testTree.postOrder((node) => {
+    //     console.log(node);
+    // })
+    testTree.insert(11);
+    testTree.insert(12);
+    testTree.insert(13);
+    testTree.insert(14);
+    testTree.insert(15);
+    testTree.insert(16);
+    testTree.insert(17);
+    testTree.insert(18);
+    testTree.insert(19);
+    testTree.insert(20);
+    
+    console.log(testTree.isBalanced());
+    testTree.prettyPrint(testTree.root);
+    testTree.rebalance();
+    console.log(testTree.isBalanced());
+    testTree.prettyPrint(testTree.root);
+    
+    
+    testTree.levelOrder((node) => {
+            console.log(node);
+        })
+        testTree.preOrder((node) => {
+                console.log(node);
+            })
+            testTree.inOrder((node) => {
+                    console.log(node);
+    })
+    testTree.postOrder((node) => {
+            console.log(node);
+        })
